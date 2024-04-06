eb1db0e0d56c33ee61f7fec3ce52c45c35fcf577
YongERong <yong_e-rong@s2017.sst.edu.sg>
2024-04-07 04:59:08 +0800

    Goal list

--- app/Dashboard/(tabs)/index.js
+++ app/Dashboard/(tabs)/index.js
@@ -7,6 +7,7 @@ import Colors from "../../../utils/Colors";
 import NavBar from "../../../components/NavBar";
 import getUsername from "../../../utils/getUsername";
 import { useState } from "react";
+import { TouchableOpacity } from "react-native-web";
 
 const Graph = () => {
   const chartConfig = {
@@ -41,6 +42,28 @@ const Graph = () => {
   );
 };
 
+const initalGoals = [
+  { id: 1, goal: "Switch to energy-efficient LED light bulbs" },
+  { id: 2, goal: "Use natural lighting whenever possible" },
+  { id: 3, goal: "Unplug electronics when not in use" },
+  { id: 4, goal: "Install a programmable thermostat" },
+  { id: 5, goal: "Seal gaps and cracks in doors and windows" },
+  { id: 6, goal: "Use energy-efficient appliances" },
+  { id: 7, goal: "Turn off lights and fans when leaving a room" },
+  { id: 8, goal: "Wash clothes in cold water" },
+  { id: 9, goal: "Air dry clothes instead of using a dryer" },
+  { id: 10, goal: "Use power strips to easily turn off multiple devices" },
+  { id: 11, goal: "Cook with lids on pots and pans to retain heat" },
+  { id: 12, goal: "Use microwave or toaster oven for small cooking tasks" },
+  { id: 13, goal: "Keep refrigerator coils clean and well-maintained" },
+  { id: 14, goal: "Lower the thermostat on your water heater" },
+  { id: 15, goal: "Plant shade trees around your home" },
+  { id: 16, goal: "Use a ceiling fan to circulate air efficiently" },
+  { id: 17, goal: "Take shorter showers to reduce water heating energy" },
+  { id: 18, goal: "Use energy-saving modes on electronics and appliances" },
+  { id: 19, goal: "Opt for energy-efficient windows and doors" },
+];
+
 const Home = (props) => {
   // const userName = String(props.username)
   // const Health = String(props.Health)
@@ -51,6 +74,8 @@ const Home = (props) => {
   const profileImg = "./assets/user.png";
   const Health = 99;
   const Gems = 1000;
+  const [goals, setGoals] = useState(initalGoals);
+  getGoals(setGoals);
 
   const leaderboard = [
     {
@@ -89,7 +114,19 @@ const Home = (props) => {
           <GemContainer Gems={Gems}></GemContainer>
         </CardView>
 
-        <CardView title={"Leaderboard"}></CardView>
+        <CardView title="Goals">
+          {goals.map(id, (goal) => (
+            <View style={styles.goalItem}>
+              <TouchableOpacity
+                style={styles.goalCompleteBtn}
+                onPress={() => {
+                  setGoals(goals.filter((a) => a.id !== goal.id));
+                }}
+              ></TouchableOpacity>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </CardView>
 
         <CardView title={"Energy Usage"}>
           <Graph style={{ width: "100%" }} />
